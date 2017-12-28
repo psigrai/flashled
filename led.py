e253400bc72c17e18d2cd36e95d8e3aaae80772f
psigrai <jongrainger@me.com>
2017-12-28 12:09:33 +0000

    added led.py - pins working and indent for GPIO corrected

--- led.py
+++ led.py
@@ -3,12 +3,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(2, GPIO.OUT)
+GPIO.setup (11, GPIO.OUT)
 
 for i in range(10):
-    GPIO.output(2, 1)
+    GPIO.output(11, 1)
     time.sleep(0.2)
-    GPIO.output(2, 0)
+    GPIO.output(11, 0)
     time.sleep(0.2)
-    GPIO.cleanup()
+GPIO.cleanup()
     
